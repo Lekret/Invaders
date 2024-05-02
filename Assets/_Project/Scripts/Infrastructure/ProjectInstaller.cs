4b517eb2479840093563d43de848197c13a5d1ec
lekret <lekret399@mail.ru>
2024-05-02 22:22:13 +0300

    Initial: Zenject, UniRx, sprites, basic structure

---
+++ Assets/_Project/Scripts/Infrastructure/ProjectInstaller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace _Project.Scripts.Infrastructure
+{
+    public class ProjectInstaller : MonoBehaviour
+    {
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Services/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Services
+{
+    public class SceneLoader
+    {
+        public void ReloadScene()
+        {
+            var activeScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(activeScene.buildIndex);
+        }
+    }
+}
