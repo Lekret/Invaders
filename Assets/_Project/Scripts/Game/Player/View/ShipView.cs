c7c52db389697d9f20b837e32340d8d527cd324a
lekret <lekret399@mail.ru>
2024-05-08 19:10:18 +0300

    Ship movement rotation

--- Assets/_Project/Scripts/Game/Player/View/ShipView.cs
+++ Assets/_Project/Scripts/Game/Player/View/ShipView.cs
@@ -10,9 +10,11 @@ namespace _Project.Scripts.Game.Player.View
     public class ShipView : MonoBehaviour
     {
         [SerializeField] private PlayableDirector _damageDirector;
+        [SerializeField] private Transform _spriteRoot;
         
         private readonly CompositeDisposable _subscriptions = new();
         private Ship _ship;
+        private Vector3 _lastPosition;
 
         public Ship Ship => _ship;
 
@@ -20,6 +22,8 @@ namespace _Project.Scripts.Game.Player.View
         {
             _ship = ship;
             transform.position = ship.Position;
+            _lastPosition = ship.Position;
+            
             ship
                 .PositionAsObservable()
                 .Subscribe(x => transform.position = x)
@@ -35,6 +39,26 @@ namespace _Project.Scripts.Game.Player.View
                 .AddTo(_subscriptions);
             
             _subscriptions.AddTo(gameObject);
+        }
+
+        private void LateUpdate()
+        {
+            var position = transform.position;
+            var lastPosition = _lastPosition;
+            _lastPosition = position;
+            var direction = position - lastPosition;
+            
+            var directionNormalized = direction != Vector3.zero ? direction.normalized : Vector3.up;
+            var targetRotationZ = directionNormalized.x switch
+            {
+                0 => 0f,
+                > 0 => -10f,
+                < 0 => 10f,
+            };
+
+            var targetRotation = Quaternion.Euler(0f, 0f, targetRotationZ);
+            _spriteRoot.localRotation = Quaternion.Lerp(_spriteRoot.localRotation, targetRotation, Time.deltaTime * 8f);
+
         }
     }
 }
